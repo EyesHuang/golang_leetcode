94f63a2f2c5738225076db500ee6354ea618b4c4
EyesHuang <charliehuang1017@gmail.com>
2024-03-29 16:25:10 +0800

    Implement solution for question 86

--- 86/86.go
+++ 86/86.go
@@ -3,5 +3,27 @@ package partition_list
 import "leetcode/util"
 
 func partition(head *util.ListNode, x int) *util.ListNode {
-	return nil
+	dummy1 := &util.ListNode{}
+	dummy2 := &util.ListNode{}
+
+	p1, p2 := dummy1, dummy2
+
+	for head != nil {
+		if head.Val < x {
+			p1.Next = head
+			p1 = p1.Next
+		} else {
+			p2.Next = head
+			p2 = p2.Next
+
+		}
+
+		tmp := head.Next
+		head.Next = nil
+		head = tmp
+	}
+
+	p1.Next = dummy2.Next
+
+	return dummy1.Next
 }
