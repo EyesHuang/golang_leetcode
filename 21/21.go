f01755eb572b909d2c82977979afcb7960fb8999
EyesHuang <charliehuang1017@gmail.com>
2024-03-29 22:35:35 +0800

    Optimize the solution for question 21

--- 21/21.go
+++ 21/21.go
@@ -6,22 +6,24 @@ func mergeTwoLists(list1 *util.ListNode, list2 *util.ListNode) *util.ListNode {
 	dummy := &util.ListNode{}
 	current := dummy
 
-	for list1 != nil && list2 != nil {
-		if list1.Val > list2.Val {
-			current.Next = list2
-			list2 = list2.Next
+	p1, p2 := list1, list2
+
+	for p1 != nil && p2 != nil {
+		if p1.Val > p2.Val {
+			current.Next = p2
+			p2 = p2.Next
 		} else {
-			current.Next = list1
-			list1 = list1.Next
+			current.Next = p1
+			p1 = p1.Next
 		}
 
 		current = current.Next
 	}
 
-	if list1 != nil {
-		current.Next = list1
+	if p1 != nil {
+		current.Next = p1
 	} else {
-		current.Next = list2
+		current.Next = p2
 	}
 
 	return dummy.Next
