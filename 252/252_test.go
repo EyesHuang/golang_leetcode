408b3c06dacef87d49fcc13fb8ce9e4ecdabcc27
EyesHuang <charliehuang1017@gmail.com>
2024-05-31 00:20:23 +0800

    Add a solution with sorting method for question 252

--- 252/252_test.go
+++ 252/252_test.go
@@ -27,10 +27,22 @@ var tests = []struct {
 	},
 }
 
-func TestCanAttendMeetings(t *testing.T) {
+func TestCanAttendMeetings_BruteForce(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			res := canAttendMeetings_bruteForce(tt.intervals)
+
+			if !reflect.DeepEqual(res, tt.expected) {
+				t.Errorf("got %v, want %v", res, tt.expected)
+			}
+		})
+	}
+}
+
+func TestCanAttendMeetings_Sorting(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := canAttendMeetings_Sorting(tt.intervals)
 
 			if !reflect.DeepEqual(res, tt.expected) {
 				t.Errorf("got %v, want %v", res, tt.expected)
