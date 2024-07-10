6892cf2d3d14b82c2a69971c74b1c155e0e12acf
EyesHuang <charliehuang1017@gmail.com>
2024-07-10 10:53:39 +0800

    Implement two-pointers method for question 15

--- 1000/100/20/15.go
+++ 1000/100/20/15.go
@@ -1,5 +1,50 @@
 package leetcode_20
 
-func threeSum(nums []int) [][]int {
-	return nil
+import "sort"
+
+// Time complexity: O(n^2)
+// Space complexity: O(n)
+func threeSum_twoPointers(nums []int) [][]int {
+	sort.Ints(nums)
+
+	res := make([][]int, 0, len(nums))
+
+	for i, val := range nums {
+		// skip positive intergers
+		if val > 0 {
+			break
+		}
+
+		// skip duplicates
+		if i > 0 && val == nums[i-1] {
+			continue
+		}
+
+		twoSum(nums, i, &res)
+	}
+
+	return res
+}
+
+func twoSum(nums []int, i int, res *[][]int) {
+	l, r := i+1, len(nums)-1
+
+	for l < r {
+		sum := nums[i] + nums[l] + nums[r]
+
+		if sum < 0 {
+			l++
+		} else if sum > 0 {
+			r--
+		} else {
+			*res = append(*res, []int{nums[i], nums[l], nums[r]})
+			l++
+			r--
+
+			// skip duplicates
+			for l < r && nums[l] == nums[l-1] {
+				l++
+			}
+		}
+	}
 }
--- 1000/100/20/15_test.go
+++ 1000/100/20/15_test.go
@@ -15,22 +15,27 @@ var q15TestCases = []struct {
 		[]int{-1, 0, 1, 2, -1, -4},
 		[][]int{{-1, -1, 2}, {-1, 0, 1}},
 	},
-	// {
-	// 	"Test 2",
-	// 	[]int{0, 1, 1},
-	// 	[][]int{},
-	// },
-	// {
-	// 	"Test 3",
-	// 	[]int{0, 0, 0},
-	// 	[][]int{{0, 0, 0}},
-	// },
+	{
+		"Test 2",
+		[]int{0, 1, 1},
+		[][]int{},
+	},
+	{
+		"Test 3",
+		[]int{0, 0, 0},
+		[][]int{{0, 0, 0}},
+	},
+	{
+		"Test 4",
+		[]int{-2, 0, 3, -1, 4, 0, 3, 4, 1, 1, 1, -3, -5, 4, 0},
+		[][]int{{-5, 1, 4}, {-3, -1, 4}, {-3, 0, 3}, {-2, -1, 3}, {-2, 1, 1}, {-1, 0, 1}, {0, 0, 0}},
+	},
 }
 
-func TestThreeSum(t *testing.T) {
+func TestThreeSumTwoPointers(t *testing.T) {
 	for _, tt := range q15TestCases {
 		t.Run(tt.name, func(t *testing.T) {
-			res := threeSum(tt.nums)
+			res := threeSum_twoPointers(tt.nums)
 
 			if !reflect.DeepEqual(res, tt.expected) {
 				t.Errorf("got %v, want %v", res, tt.expected)
